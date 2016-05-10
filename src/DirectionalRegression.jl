8fcdee10dc0b2905e3ea45702e3021558b7403b6
roger <roger.herikstad@gmail.com>
2016-05-10 15:38:55 +0800

    Removed test function that belong in runtests

--- src/DirectionalRegression.jl
+++ src/DirectionalRegression.jl
@@ -54,11 +54,6 @@ function get_κ(θ::Array{Float64,1}, args...)
 	get_κ(sqrt(real(N/(N-1)*(R2 - 1/N))),args...)
 end
 
-function test_get_κ()
-	κ = get_κ(I1(1.5)/I0(1.5))
-	Base.Test.@test_approx_eq κ 1.5	
-end
-
 function loglikehood(θ::Array{Float64,1}, κ::Real, μ::Real, β::Array{Float64,1},X::Array{Float64,2})
   ncells,ntrials = size(X)
   LL = -2*pi*ntrials*log(I0(κ))
@@ -151,43 +146,6 @@ function func4(β::Array{Float64,1},X::Array{Float64,2},θ::Array{Float64,1},μ:
 	dβ
 end
 
-
-function test_func3_vs_func4()
-	_β = rand(20) - 0.5
-	_μ = 0.0
-	_Aκ = 0.9
-	_X = rand(1000,20)
-	_Xt = _X'
-	_θ = 2*pi*rand(1000) - pi;
-	dβ1 = func4(_β, _X, _θ, _μ, _Aκ)
-	dβ2 = func3(_β, _Xt, _θ, _μ, _Aκ) 
-	Base.Test.@test_approx_eq dβ1 dβ2
-end
-
-"""
-Check that the IRWLS algorithm converges to the correct β if we supply the correct μ and κ
-	"""
-function test_func2()
-	X = 2*rand(1000,3)-1;
-	β = [0.181689,0.533594,0.132475]
-	μ0 = 2*atan(X*β);
-	θ = zeros(size(X,1))
-	κ = 2.0
-	Aκ  = I1(κ)/I0(κ)
-	θ
-	for i in 1:length(θ)
-		 θ[i] = rand(sampler(VonMises(μ0[i], κ)))
-	 end
-	 βn = 2*rand(size(X,2))-1
-	 for i in 1:10
-		 δβ = func2(βn, X', θ, 0.0, Aκ)
-		 for j in 1:length(β)
-			 βn[j] += δβ[j]
-		 end
-	 end
-	 print(βn)
-	 Base.Test.@test sumabs2(β - βn) < 1e-2
-end
 
 function func_s(β::Array{Float64,1}, X::Array{Float64,2}, θ::Array{Float64,1})
   s = 0.0
